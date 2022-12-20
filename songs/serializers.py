c806d9f9bf71464fd1d7cae1dedeb266fbf58ba4
=Welton Santos <weltonsantosfr@gmail.com>
2022-12-20 12:45:37 -0300

    edit requirements.txt

--- songs/serializers.py
+++ songs/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from .models import Song
+from albums.serializers import AlbumSerializer
 
 
 class SongSerializer(serializers.ModelSerializer):
+    album = AlbumSerializer(read_only=True)
     class Meta:
         model=Song
         fields="__all__"
